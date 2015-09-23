38a914b94e882e592f455cf717ebd43f31b26358
Kenton Hamaluik <hamaluik@ualberta.ca>
2015-09-23 17:03:51 -0600

    Playing around with socket.io

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var openid = require('openid');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
-  res.send('Hello stori!');
+app.use(express.static('template'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
 });
 
-var server = app.listen(app.get('port'), function() {
-  console.log('stori is running on port', app.get('port'));
+http.listen(app.get('port'), function(){
+  console.log('listening on *:' + app.get('port'));
 });
