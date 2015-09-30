ec0dab45a406d8d6e4902e9f62f91789d7a8b2ab
Kenton Hamaluik <hamaluik@ualberta.ca>
2015-09-30 09:05:04 -0600

    Playing with angular

--- template/js/stori.js
+++ template/js/stori.js
@@ -1,5 +1,24 @@
-var storiApp = angular.module('storiApp', []);
+var storiApp = angular.module('storiApp', ['ui.router']);
 
-storiApp.controller('mainController', function($scope) {
-	$scope.message = 'Main controller saying hi!';
+storiApp.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /login
+  $urlRouterProvider.otherwise("/projects");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('login', {
+      url: "/login",
+      templateUrl: "views/login.html"
+    })
+    .state('projects', {
+      url: "/projects",
+      templateUrl: "views/projects.html",
+      controller: function($scope) {
+      	$scope.projects = [
+      		{ name: "Media 101", description: "Virtual reality media training" },
+      		{ name: "MedBIKE", description: "Cycling excerise enhanced by virtual reality." }
+      	];
+      }
+    });
 });
